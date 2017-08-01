1bb176d7d36d2237fd8985fb1e9b72c84446ecfb
[layers1995] <lsayers@knox.edu>
2017-07-31 19:08:39 -0500

    fix foreign key names to be singular

--- db/migrate/20170717222726_add_foreign_key_to_airport.rb
+++ db/migrate/20170717222726_add_foreign_key_to_airport.rb
@@ -1,7 +1,5 @@
 class AddForeignKeyToAirport < ActiveRecord::Migration[5.0]
   def change
-  	#add_foreign_key :airports, :cities
-  	#remove_reference :airports, :cities, index: true, foreign_key: true # Yes, this is dumb. I fucked up	
-  	add_reference :airports, :cities, index: true, foreign_key: true
+  	add_reference :airports, :city, index: true, foreign_key: true
   end
 end
--- db/migrate/20170723164123_change_classification_types_id_to_classifications_id_in_categories.rb
+++
@@ -1,5 +0,0 @@
-class ChangeClassificationTypesIdToClassificationsIdInCategories < ActiveRecord::Migration[5.0]
-  def change
-  	rename_column :categories, :classification_types_id, :classifications_id
-  end
-end
