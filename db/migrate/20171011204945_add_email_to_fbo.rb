276a84cc9215bf2a9600e4be15c883264572798e
[layers1995] <lsayers@knox.edu>
2017-10-22 22:27:44 -0500

    try to fix email migration

--- db/migrate/20171011204945_add_email_to_fbo.rb
+++ db/migrate/20171011204945_add_email_to_fbo.rb
@@ -1,6 +1,6 @@
 class AddEmailToFbo < ActiveRecord::Migration[5.0]
   def change
   	Fbo.reset_column_information
-  	add_column(:fbos, :email, :string) unless Fbo.column_names_include?('email')
+  	add_column(:fbos, :email, :string) unless Fbo.column_names.include?('email')
   end
 end
